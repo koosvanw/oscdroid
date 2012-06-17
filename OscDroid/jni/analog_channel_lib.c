57f2f1e3cf37be5fdf1806b5e08c8e255546a378
koosvanw@gmail.com <koosvanw@gmail.com@0528ea6d-7168-1de9-e85b-448c7b122c18>
2012-06-17 19:05:14 +0000

    Change y_calculation in jni. To be tested;

--- OscDroid/jni/analog_channel_lib.c
+++ OscDroid/jni/analog_channel_lib.c
@@ -15,8 +15,9 @@ jfloat Java_com_kvw_oscdroid_channels_AnalogChannel_calcDisplayX(JNIEnv *env, jo
 jfloat Java_com_kvw_oscdroid_channels_AnalogChannel_calcDisplayY(JNIEnv *env, jobject this, jint dataPoint, jfloat scrnHeight, jfloat zoomY, jfloat offsetY)
 {
 	jfloat y=0;
-
-	y = (scrnHeight + zoomY)/256*(255-dataPoint)+ offsetY;
+	jfloat tmp = (dataPoint-128)*zoomY+128;
+		
+	y = (scrnHeight)/256*(255-dataPoint)+ offsetY;
 
 	return y;
 }
